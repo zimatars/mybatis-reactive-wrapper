9e2232383bef8fc4fd7c34d3e4a3fa13e2297477
RoundSoupBubble <11007575+RoundSoupBubble@users.noreply.github.com>
2021-12-02 23:06:25 +0800

    update MapperProxy

--- src/main/java/org/apache/ibatis/binding/MapperProxy.java
+++ src/main/java/org/apache/ibatis/binding/MapperProxy.java
@@ -1,17 +1,17 @@
 /**
- *    Copyright 2009-2021 the original author or authors.
- *
- *    Licensed under the Apache License, Version 2.0 (the "License");
- *    you may not use this file except in compliance with the License.
- *    You may obtain a copy of the License at
- *
- *       http://www.apache.org/licenses/LICENSE-2.0
- *
- *    Unless required by applicable law or agreed to in writing, software
- *    distributed under the License is distributed on an "AS IS" BASIS,
- *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *    See the License for the specific language governing permissions and
- *    limitations under the License.
+ * Copyright 2009-2021 the original author or authors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package org.apache.ibatis.binding;
 
@@ -41,145 +41,145 @@ import java.util.Map;
  */
 public class MapperProxy<T> implements InvocationHandler, Serializable {
 
-  private static final long serialVersionUID = -4724728412955527868L;
-  private static final int ALLOWED_MODES = Lookup.PRIVATE | Lookup.PROTECTED
-      | Lookup.PACKAGE | Lookup.PUBLIC;
-  private static final Constructor<Lookup> lookupConstructor;
-  private static final Method privateLookupInMethod;
-  private final SqlSession sqlSession;
-  private final Class<T> mapperInterface;
-  private final Map<Method, MapperMethodInvoker> methodCache;
+    private static final long serialVersionUID = -4724728412955527868L;
+    private static final int ALLOWED_MODES = Lookup.PRIVATE | Lookup.PROTECTED
+            | Lookup.PACKAGE | Lookup.PUBLIC;
+    private static final Constructor<Lookup> lookupConstructor;
+    private static final Method privateLookupInMethod;
+    private final SqlSession sqlSession;
+    private final Class<T> mapperInterface;
+    private final Map<Method, MapperMethodInvoker> methodCache;
 
-  public MapperProxy(SqlSession sqlSession, Class<T> mapperInterface, Map<Method, MapperMethodInvoker> methodCache) {
-    this.sqlSession = sqlSession;
-    this.mapperInterface = mapperInterface;
-    this.methodCache = methodCache;
-  }
-
-  static {
-    Method privateLookupIn;
-    try {
-      privateLookupIn = MethodHandles.class.getMethod("privateLookupIn", Class.class, Lookup.class);
-    } catch (NoSuchMethodException e) {
-      privateLookupIn = null;
+    public MapperProxy(SqlSession sqlSession, Class<T> mapperInterface, Map<Method, MapperMethodInvoker> methodCache) {
+        this.sqlSession = sqlSession;
+        this.mapperInterface = mapperInterface;
+        this.methodCache = methodCache;
     }
-    privateLookupInMethod = privateLookupIn;
 
-    Constructor<Lookup> lookup = null;
-    if (privateLookupInMethod == null) {
-      // JDK 1.8
-      try {
-        lookup = Lookup.class.getDeclaredConstructor(Class.class, int.class);
-        lookup.setAccessible(true);
-      } catch (NoSuchMethodException e) {
-        throw new IllegalStateException(
-            "There is neither 'privateLookupIn(Class, Lookup)' nor 'Lookup(Class, int)' method in java.lang.invoke.MethodHandles.",
-            e);
-      } catch (Exception e) {
-        lookup = null;
-      }
-    }
-    lookupConstructor = lookup;
-  }
-
-  @Override
-  public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
-    try {
-      if (Object.class.equals(method.getDeclaringClass())) {
-        return method.invoke(this, args);
-      } else {
-        return cachedInvoker(method).invoke(proxy, method, args, sqlSession);
-      }
-    } catch (Throwable t) {
-      throw ExceptionUtil.unwrapThrowable(t);
-    }
-  }
-
-  private MapperMethodInvoker cachedInvoker(Method method) throws Throwable {
-    try {
-      return MapUtil.computeIfAbsent(methodCache, method, m -> {
-        if (m.isDefault()) {
-          try {
-            if (privateLookupInMethod == null) {
-              return new DefaultMethodInvoker(getMethodHandleJava8(method));
-            } else {
-              return new DefaultMethodInvoker(getMethodHandleJava9(method));
-            }
-          } catch (IllegalAccessException | InstantiationException | InvocationTargetException
-              | NoSuchMethodException e) {
-            throw new RuntimeException(e);
-          }
-        } else {
-          return new PlainMethodInvoker(new MapperMethod(mapperInterface, method, sqlSession.getConfiguration()));
+    static {
+        Method privateLookupIn;
+        try {
+            privateLookupIn = MethodHandles.class.getMethod("privateLookupIn", Class.class, Lookup.class);
+        } catch (NoSuchMethodException e) {
+            privateLookupIn = null;
         }
-      });
-    } catch (RuntimeException re) {
-      Throwable cause = re.getCause();
-      throw cause == null ? re : cause;
-    }
-  }
+        privateLookupInMethod = privateLookupIn;
 
-  private MethodHandle getMethodHandleJava9(Method method)
-      throws NoSuchMethodException, IllegalAccessException, InvocationTargetException {
-    final Class<?> declaringClass = method.getDeclaringClass();
-    return ((Lookup) privateLookupInMethod.invoke(null, declaringClass, MethodHandles.lookup())).findSpecial(
-        declaringClass, method.getName(), MethodType.methodType(method.getReturnType(), method.getParameterTypes()),
-        declaringClass);
-  }
-
-  private MethodHandle getMethodHandleJava8(Method method)
-      throws IllegalAccessException, InstantiationException, InvocationTargetException {
-    final Class<?> declaringClass = method.getDeclaringClass();
-    return lookupConstructor.newInstance(declaringClass, ALLOWED_MODES).unreflectSpecial(method, declaringClass);
-  }
-
-  interface MapperMethodInvoker {
-    Object invoke(Object proxy, Method method, Object[] args, SqlSession sqlSession) throws Throwable;
-  }
-
-  private static class PlainMethodInvoker implements MapperMethodInvoker {
-    private final MapperMethod mapperMethod;
-    private final static TransactionSynchronizationManager noneInstance = new TransactionSynchronizationManager(null);
-
-    public PlainMethodInvoker(MapperMethod mapperMethod) {
-      super();
-      this.mapperMethod = mapperMethod;
+        Constructor<Lookup> lookup = null;
+        if (privateLookupInMethod == null) {
+            // JDK 1.8
+            try {
+                lookup = Lookup.class.getDeclaredConstructor(Class.class, int.class);
+                lookup.setAccessible(true);
+            } catch (NoSuchMethodException e) {
+                throw new IllegalStateException(
+                        "There is neither 'privateLookupIn(Class, Lookup)' nor 'Lookup(Class, int)' method in java.lang.invoke.MethodHandles.",
+                        e);
+            } catch (Exception e) {
+                lookup = null;
+            }
+        }
+        lookupConstructor = lookup;
     }
 
     @Override
-    public Object invoke(Object proxy, Method method, Object[] args, SqlSession sqlSession) throws Throwable {
-      Mono<Object> retMono = TransactionSynchronizationManager.forCurrentTransaction()
-              .onErrorReturn(NoTransactionException.class,noneInstance)
-              .publishOn(MybatisScheduler.getScheduler())
-              .map( synchronizationManager -> {
-                            MybatisTransactionScope.setTransactionSynchronizationManager(synchronizationManager);
-                            if(synchronizationManager==noneInstance) MybatisTransactionScope.clearTransactionSynchronizationManager();
-                            try {
-                              return mapperMethod.execute(sqlSession, args);
-                            }finally {
-                              MybatisTransactionScope.clearTransactionSynchronizationManager();
-                            }
-                  });
-      if(mapperMethod.getMethodSignature().returnsMany()){
-        return retMono.flatMapIterable(it -> (Iterable<?>) it);
-      }else {
-        return retMono;
-      }
+    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+        try {
+            if (Object.class.equals(method.getDeclaringClass())) {
+                return method.invoke(this, args);
+            } else {
+                return cachedInvoker(method).invoke(proxy, method, args, sqlSession);
+            }
+        } catch (Throwable t) {
+            throw ExceptionUtil.unwrapThrowable(t);
+        }
     }
 
-  }
-
-  private static class DefaultMethodInvoker implements MapperMethodInvoker {
-    private final MethodHandle methodHandle;
-
-    public DefaultMethodInvoker(MethodHandle methodHandle) {
-      super();
-      this.methodHandle = methodHandle;
+    private MapperMethodInvoker cachedInvoker(Method method) throws Throwable {
+        try {
+            return MapUtil.computeIfAbsent(methodCache, method, m -> {
+                if (m.isDefault()) {
+                    try {
+                        if (privateLookupInMethod == null) {
+                            return new DefaultMethodInvoker(getMethodHandleJava8(method));
+                        } else {
+                            return new DefaultMethodInvoker(getMethodHandleJava9(method));
+                        }
+                    } catch (IllegalAccessException | InstantiationException | InvocationTargetException
+                            | NoSuchMethodException e) {
+                        throw new RuntimeException(e);
+                    }
+                } else {
+                    return new PlainMethodInvoker(new MapperMethod(mapperInterface, method, sqlSession.getConfiguration()));
+                }
+            });
+        } catch (RuntimeException re) {
+            Throwable cause = re.getCause();
+            throw cause == null ? re : cause;
+        }
     }
 
-    @Override
-    public Object invoke(Object proxy, Method method, Object[] args, SqlSession sqlSession) throws Throwable {
-      return methodHandle.bindTo(proxy).invokeWithArguments(args);
+    private MethodHandle getMethodHandleJava9(Method method)
+            throws NoSuchMethodException, IllegalAccessException, InvocationTargetException {
+        final Class<?> declaringClass = method.getDeclaringClass();
+        return ((Lookup) privateLookupInMethod.invoke(null, declaringClass, MethodHandles.lookup())).findSpecial(
+                declaringClass, method.getName(), MethodType.methodType(method.getReturnType(), method.getParameterTypes()),
+                declaringClass);
     }
-  }
+
+    private MethodHandle getMethodHandleJava8(Method method)
+            throws IllegalAccessException, InstantiationException, InvocationTargetException {
+        final Class<?> declaringClass = method.getDeclaringClass();
+        return lookupConstructor.newInstance(declaringClass, ALLOWED_MODES).unreflectSpecial(method, declaringClass);
+    }
+
+    interface MapperMethodInvoker {
+        Object invoke(Object proxy, Method method, Object[] args, SqlSession sqlSession) throws Throwable;
+    }
+
+    private static class PlainMethodInvoker implements MapperMethodInvoker {
+        private final MapperMethod mapperMethod;
+        private final static TransactionSynchronizationManager noneInstance = new TransactionSynchronizationManager(null);
+
+        public PlainMethodInvoker(MapperMethod mapperMethod) {
+            super();
+            this.mapperMethod = mapperMethod;
+        }
+
+        @Override
+        public Object invoke(Object proxy, Method method, Object[] args, SqlSession sqlSession) throws Throwable {
+            Mono<Object> retMono = TransactionSynchronizationManager.forCurrentTransaction()
+                    .onErrorReturn(NoTransactionException.class, noneInstance)
+                    .flatMap(synchronizationManager -> Mono.fromSupplier(() -> {
+                        MybatisTransactionScope.setTransactionSynchronizationManager(synchronizationManager);
+                        if (synchronizationManager == noneInstance)
+                            MybatisTransactionScope.clearTransactionSynchronizationManager();
+                        try {
+                            return mapperMethod.execute(sqlSession, args);
+                        } finally {
+                            MybatisTransactionScope.clearTransactionSynchronizationManager();
+                        }
+                    }).subscribeOn(MybatisScheduler.getScheduler()));
+            if (mapperMethod.getMethodSignature().returnsMany()) {
+                return retMono.flatMapIterable(it -> (Iterable<?>) it);
+            } else {
+                return retMono;
+            }
+        }
+
+    }
+
+    private static class DefaultMethodInvoker implements MapperMethodInvoker {
+        private final MethodHandle methodHandle;
+
+        public DefaultMethodInvoker(MethodHandle methodHandle) {
+            super();
+            this.methodHandle = methodHandle;
+        }
+
+        @Override
+        public Object invoke(Object proxy, Method method, Object[] args, SqlSession sqlSession) throws Throwable {
+            return methodHandle.bindTo(proxy).invokeWithArguments(args);
+        }
+    }
 }
